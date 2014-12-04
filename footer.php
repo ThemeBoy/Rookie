45f08521fa72f9ca7041e0a54f1a9ee8b2b92410
ThemeBoy <support@themeboy.com>
2014-12-04 22:50:33 +1100

    Style footer links

--- footer.php
+++ footer.php
@@ -12,9 +12,9 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'rookie' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'rookie' ), 'WordPress' ); ?></a>
+			<a href="http://wordpress.org/"><?php printf( __( 'Proudly powered by %s', 'rookie' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'rookie' ), 'Rookie', '<a href="http://themeboy.com" rel="designer">ThemeBoy</a>' ); ?>
+			<a href="http://themeboy.com/"><?php printf( __( 'Designed by %s', 'rookie' ), 'ThemeBoy' ); ?></a>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
