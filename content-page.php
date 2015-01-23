faca94f4aab4468c8342468000b255bad3bec5e2
Brian Miyaji <brian@themeboy.com>
2015-01-24 01:48:31 +1100

    Remove link on page title

--- content-page.php
+++ content-page.php
@@ -7,17 +7,15 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<?php if ( ! is_single() ) { ?><a href="<?php echo esc_url( get_permalink() ); ?>"><?php } ?>
-		<header class="entry-header">
-			<?php
-			if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
-				the_post_thumbnail( 'large' );
-			} 
-			?>
-			
-			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-		</header><!-- .entry-header -->
-	<?php if ( ! is_single() ) { ?></a><?php } ?>
+	<header class="entry-header">
+		<?php
+		if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
+			the_post_thumbnail( 'large' );
+		} 
+		?>
+		
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	</header><!-- .entry-header -->
 
 	<div class="entry-content">
 		<?php the_content(); ?>
