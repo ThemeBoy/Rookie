f470c34b28ede4119943e8dc0dedddc28bb46ecf
Brian Miyaji <brian@themeboy.com>
2015-04-25 15:59:25 +1000

    Add full-width selector

--- template-fullwidth.php
+++ template-fullwidth.php
@@ -9,7 +9,7 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
+	<div id="primary" class="content-area content-area-full-width">
 		<main id="main" class="site-main" role="main">
 
 			<?php while ( have_posts() ) : the_post(); ?>
