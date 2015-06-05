c82373f34660a9876384f3f75ca6711b45e76d6c
Brian Miyaji <brian@themeboy.com>
2015-06-05 23:34:42 +1000

    Wrap entry thumbnail for styling

--- content-single.php
+++ content-single.php
@@ -8,11 +8,11 @@
 	<?php if ( ! is_single() ) { ?><a href="<?php echo esc_url( get_permalink() ); ?>"><?php } ?>
 
 	<header class="entry-header">
-		<?php
-		if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
-			the_post_thumbnail( 'large' );
-		} 
-		?>
+		<?php if ( has_post_thumbnail() ) { ?>
+			<div class="entry-thumbnail">
+				<?php the_post_thumbnail( 'large' ); ?>
+			</div>
+		<?php } ?>
 
 		<?php rookie_entry_meta(); ?>
 		<?php rookie_entry_date(); ?>
