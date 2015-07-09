ffa1bf1e89d997002876f36ae0adfdaf40a2b3c5
Brian Miyaji <brian@themeboy.com>
2015-07-09 19:22:56 +1000

    use main element

--- 404.php
+++ 404.php
@@ -8,7 +8,7 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<div id="main" class="site-main" role="main">
+		<main id="main" class="site-main" role="main">
 
 			<section class="error-404 not-found">
 				<header class="page-header">
@@ -22,7 +22,7 @@ get_header(); ?>
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
 
-		</div><!-- #main -->
+		</main><!-- #main -->
 	</div><!-- #primary -->
 
 <?php get_footer(); ?>
