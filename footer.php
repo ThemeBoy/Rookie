54d2a907a4a97e3e83562316973b93e8e4ad2ee1
Brian Miyaji <brian@themeboy.com>
2015-08-03 22:15:44 +1000

    Remove footer logo and style 3 widgets

--- footer.php
+++ footer.php
@@ -15,12 +15,8 @@ if ( array_key_exists( 'logo_url', $options ) && ! empty( $options['logo_url'] )
 
 	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer<?php if ( isset( $logo ) ) { ?> site-footer-has-logo<?php } ?>" role="contentinfo">
+	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="footer-area">
-			<?php if ( isset( $logo ) ) { ?>
-				<div class="footer-logo"><img src="<?php echo $logo; ?>" alt="<?php bloginfo( 'name' ); ?>"></div>
-			<?php } ?>
-
 			<div id="quaternary" class="footer-widgets" role="complementary">
 				<?php for ( $i = 1; $i <= 3; $i++ ) { ?>
 					<div class="footer-widget-region"><?php dynamic_sidebar( sprintf( 'footer-%d', $i ) ); ?></div>
