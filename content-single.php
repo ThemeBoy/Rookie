cb5176ef48ef91946b591e094c462b91de1e5c72
Brian Miyaji <brian@themeboy.com>
2015-08-17 14:25:50 +1000

    Tweak entry meta styling

--- content-single.php
+++ content-single.php
@@ -7,30 +7,36 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<?php if ( ! is_single() ) { ?><a href="<?php echo esc_url( get_permalink() ); ?>"><?php } ?>
 
-	<header class="entry-header">
-		<?php if ( has_post_thumbnail() ) { ?>
-			<div class="entry-thumbnail">
-				<?php the_post_thumbnail( 'large' ); ?>
+	<?php if ( has_post_thumbnail() ) { ?>
+		<div class="entry-thumbnail">
+			<?php the_post_thumbnail( 'large' ); ?>
+		</div>
+	<?php } ?>
+
+	<div class="single-entry">
+		<header class="entry-header">
+			<?php the_title( '<h1 class="entry-title single-entry-title">', '</h1>' ); ?>
+
+			<div class="entry-details">
+				<?php do_action( 'rookie_before_entry_details' ); ?>
+				<?php rookie_entry_meta(); ?>
+				<?php rookie_entry_date(); ?>
+				<?php do_action( 'rookie_entry_details' ); ?>
 			</div>
-		<?php } ?>
+		</header><!-- .entry-header -->
 
-		<?php rookie_entry_meta(); ?>
-		<?php rookie_entry_date(); ?>
+		<?php if ( ! is_single() ) { ?></a><?php } ?>
 
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
+		<div class="entry-content">
+			<?php the_content(); ?>
+			<?php
+				wp_link_pages( array(
+					'before' => '<div class="page-links">' . __( 'Pages:', 'rookie' ),
+					'after'  => '</div>',
+				) );
+			?>
+		</div><!-- .entry-content -->
 
-	<?php if ( ! is_single() ) { ?></a><?php } ?>
-
-	<div class="entry-content">
-		<?php the_content(); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'rookie' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
-
-	<?php rookie_entry_footer(); ?>
+		<?php rookie_entry_footer(); ?>
+	</div>
 </article><!-- #post-## -->
--- content.php
+++ content.php
@@ -11,30 +11,36 @@
 		</a>
 	<?php } ?>
 
-	<header class="article-header">
-		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+	<div class="single-article">
+		<header class="article-header">
+			<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
 
-		<?php if ( 'post' == get_post_type() ) : ?>
-			<?php rookie_entry_date(); ?>
-		<?php endif; ?>
-	</header><!-- .article-header -->
+			<?php if ( 'post' == get_post_type() ) : ?>
+				<div class="article-details">
+					<?php do_action( 'rookie_before_article_details' ); ?>
+					<?php rookie_entry_date(); ?>
+					<?php do_action( 'rookie_article_details' ); ?>
+				</div>
+			<?php endif; ?>
+		</header><!-- .article-header -->
 
-	<div class="entry-content article-content">
-		<?php
-			/* translators: %s: Name of current post */
-			the_content( sprintf(
-				__( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'rookie' ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			) );
-		?>
+		<div class="entry-content article-content">
+			<?php
+				/* translators: %s: Name of current post */
+				the_content( sprintf(
+					__( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'rookie' ),
+					the_title( '<span class="screen-reader-text">"', '"</span>', false )
+				) );
+			?>
 
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'rookie' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
+			<?php
+				wp_link_pages( array(
+					'before' => '<div class="page-links">' . __( 'Pages:', 'rookie' ),
+					'after'  => '</div>',
+				) );
+			?>
+		</div><!-- .entry-content -->
 
-	<?php rookie_entry_footer(); ?>
+		<?php rookie_entry_footer(); ?>
+	</div>
 </article><!-- #post-## -->
