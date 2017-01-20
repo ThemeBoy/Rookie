a991477c13dcdaf8522b6d6a9c8f80382e01632a
Brian Miyaji <brian@themeboy.com>
2017-01-20 12:14:52 +1100

    Fix customizer panel scripts for WP 4.7

--- js/customize-panel.js
+++ js/customize-panel.js
@@ -1,11 +1,11 @@
 (function($) {
 	// Trigger colors section.
-	$('body').on('click', '#accordion-section-colors', function() {
-		$('#accordion-section-colors #customize-control-themeboy_customize').change();
+	$('body').on('click', '[id$="accordion-section-colors"]', function() {
+		$('[id$="accordion-section-colors"] #customize-control-themeboy_customize').change();
 	});
 	// Hide advanced custom colors when option is off.
-	$('body').on('change', '#accordion-section-colors #customize-control-themeboy_customize', function() {
-		$el = $('#accordion-section-colors #customize-control-themeboy_primary, #accordion-section-colors #customize-control-themeboy_background, #accordion-section-colors #customize-control-themeboy_text, #accordion-section-colors #customize-control-themeboy_heading, #accordion-section-colors #customize-control-themeboy_link');
+	$('body').on('change', '[id$="accordion-section-colors"] #customize-control-themeboy_customize', function() {
+		$el = $('[id$="accordion-section-colors"] #customize-control-themeboy_primary, [id$="accordion-section-colors"] #customize-control-themeboy_background, [id$="accordion-section-colors"] #customize-control-themeboy_text, [id$="accordion-section-colors"] #customize-control-themeboy_heading, [id$="accordion-section-colors"] #customize-control-themeboy_link');
 		if ( $(this).find('input').prop('checked') ) {
 			$el.show();
 		} else {
@@ -14,16 +14,17 @@
 	});
 	
 	// Trigger layout section.
-	$('body').on('click', '#accordion-section-rookie_layout', function() {
-		$('#accordion-section-rookie_layout #customize-control-themeboy_content_width input').change();
+	$('body').on('click', '[id$="accordion-section-rookie_layout"]', function() {
+		$('[id$="accordion-section-rookie_layout"] #customize-control-themeboy_content_width input').change();
 	});
 	// Update content width display
-	$('body').on('change', '#accordion-section-rookie_layout #customize-control-themeboy_content_width input', function() {
-		$el = $('#accordion-section-rookie_layout #customize-control-themeboy_content_width .customize-control-description');
+	$('body').on('change', '[id$="accordion-section-rookie_layout"] #customize-control-themeboy_content_width input', function() {
+		$el = $('[id$="accordion-section-rookie_layout"] #customize-control-themeboy_content_width .customize-control-description');
 		$el.css('float', 'right').find('span').html($(this).val()+'px').css('line-height', '24px');
 	});
-	$('body').on('mouseup', '#accordion-section-rookie_layout #customize-control-themeboy_content_width .customize-control-description a', function() {
-		$input = $('#accordion-section-rookie_layout #customize-control-themeboy_content_width input');
+	$('body').on('mouseup', '[id$="accordion-section-rookie_layout"] #customize-control-themeboy_content_width .customize-control-description a', function() {
+		$input = $('[id$="accordion-section-rookie_layout"] #customize-control-themeboy_content_width input');
+		console.log($input);
 		if ( '#minus' === $(this).attr('href') ) {
 			$input.val(function( index, val) {
 				console.log(val);
