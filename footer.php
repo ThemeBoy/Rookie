a29a373aa5b56cc2409dc9de7be384e8139c2c50
Brian Miyaji <brian@themeboy.com>
2017-06-13 14:33:32 +1000

    Add wrappers and clean up tab spacing

--- footer.php
+++ footer.php
@@ -8,28 +8,33 @@
  */
 $options = get_option( 'themeboy', array() );
 if ( array_key_exists( 'logo_url', $options ) && ! empty( $options['logo_url'] ) ) {
-	$logo = $options['logo_url'];
-	$logo = esc_url( $logo );
+  $logo = $options['logo_url'];
+  $logo = esc_url( $logo );
 }
 ?>
 
-	</div><!-- #content -->
+    </div><!-- .content-wrapper -->
+  </div><!-- #content -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="footer-area">
-			<div id="quaternary" class="footer-widgets" role="complementary">
-				<?php for ( $i = 1; $i <= 3; $i++ ) { ?>
-					<div class="footer-widget-region"><?php dynamic_sidebar( sprintf( 'footer-%d', $i ) ); ?></div>
-				<?php } ?>
-			</div>
-		</div><!-- .footer-area -->
-	</footer><!-- #colophon -->
+  <footer id="colophon" class="site-footer" role="contentinfo">
+    <div class="footer-wrapper">
+      <div class="footer-area">
+        <div id="quaternary" class="footer-widgets" role="complementary">
+          <?php for ( $i = 1; $i <= 3; $i++ ) { ?>
+            <div class="footer-widget-region"><?php dynamic_sidebar( sprintf( 'footer-%d', $i ) ); ?></div>
+          <?php } ?>
+        </div>
+      </div><!-- .footer-area -->
+    </div><!-- .footer-wrapper -->
+  </footer><!-- #colophon -->
 </div><!-- #page -->
 
 <div class="site-info">
-	<div class="info-area">
-		<?php rookie_footer(); ?>
-	</div><!-- .info-area -->
+  <div class="info-wrapper">
+    <div class="info-area">
+      <?php rookie_footer(); ?>
+    </div><!-- .info-area -->
+  </div><!-- .info-wrapper -->
 </div><!-- .site-info -->
 
 <?php wp_footer(); ?>
